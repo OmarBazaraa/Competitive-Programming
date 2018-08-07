00e48332b67d2694fbe39f18dcb8887cd0a97e06
OmarBazaraa <omar_bazaraa@hotmail.com>
2018-08-07 22:22:18 +0200

    Add template workspace with simple C++ compilation script

---
+++ workspace/run.cmd
@@ -0,0 +1,3 @@
+cls
+g++ -O2 -std=c++14 src.cpp -o src.exe
+src.exe < input.txt
---
+++ workspace/stress.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Generates random sample test case to a text file.
+void generateRandomCase() {
+    ofstream fout("input.txt");
+
+    const int N = 1e9;
+    const int L = 1e4;
+
+    int a = rand() % N + L;
+    int b = rand() % N + L;
+
+    fout << a << ' ' << b << endl;
+
+    fout.close();
+}
+
+int main() {
+    // Compile both programs: the correct and the wrong ones.
+    system("g++ -O2 -std=c++14 acc.cpp -o ac.exe");
+    system("g++ -O2 -std=c++14 src.cpp -o wa.exe");
+
+    // Set random seed
+    srand(time(0));
+
+    // Local variables
+    int testCase = 0;
+    string wa, ac;
+
+    while (wa == ac) {
+        // Print test case number
+        cout << "Test Case #" << ++testCase << endl;
+
+        // Generate random test case
+        generateRandomCase();
+
+        // Run the AC program to read from "input.txt" and output to "ac.txt"
+        system("ac.exe < input.txt > ac.txt");
+        ifstream acf("ac.txt");
+        getline(acf, ac, char(EOF));
+        acf.close();
+
+        // Run the WA program to read from "input.txt" and output to "wa.txt"
+        system("wa.exe < input.txt > wa.txt");
+        ifstream waf("wa.txt");
+        getline(waf, wa, char(EOF));
+        waf.close();
+    }
+
+    return 0;
+}
