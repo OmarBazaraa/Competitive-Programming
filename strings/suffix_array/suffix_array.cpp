355022e128f65f26c1646e33f57fb763eb785a17
OmarBazaraa <omar_bazaraa@hotmail.com>
2018-09-22 21:13:13 +0200

    Edit suffix array code and add LCP function

--- strings/suffix_array/suffix_array.cpp
+++ strings/suffix_array/suffix_array.cpp
@@ -17,15 +17,20 @@ using namespace std;
 // The maximum length of the string
 const int N = 1e5 + 5;
 
-// SA:      the suffix array, holding all the suffixes in lexicographical order.
-// bucket:  array holding the bucket (i.e. the order) of the i-th suffix after sorting.
-int SA[N], bucket[N];
+// n:           the number of suffixes (length of the string + 1)
+// SA:          the suffix array, holding all the suffixes in lexicographical order.
+// suffixRank:  array holding the rank (i.e. the order) of the i-th suffix after sorting.
+// LCP:         array holding the length of the longest common prefix between SA[i] and SA[i - 1].
+int n, SA[N], suffixRank[N], LCP[N];
+
+// Temporary arrays needed while computing the suffix array
+int sortedSA[N], sortedRanks[N], rankStart[N];
 
 /**
  * Comparator struct needed to sort the suffixes on the first 2h,
  * given they are already sorted on the first h letters.
  *
- * This struct uses bucket array in its comparisons.
+ * This struct uses suffixRank array in its comparisons.
  */
 struct comparator {
     int h;
@@ -33,73 +38,109 @@ struct comparator {
     comparator(int h) : h(h) {}
 
     bool operator()(int i, int j) const {
-        if (bucket[i] != bucket[j])
-            return bucket[i] < bucket[j];
-        return bucket[i + h] < bucket[j + h];
+        if (suffixRank[i] != suffixRank[j])
+            return suffixRank[i] < suffixRank[j];
+        return suffixRank[i + h] < suffixRank[j + h];
     }
 };
+
+/**
+ * Computes the ranks of the suffixes in the current sort iteration,
+ * and where each of these ranks starts in the suffix array.
+ * Also copies the sorted suffixes into the global suffix array (i.e. SA).
+ *
+ * Upon calling this function:
+ * @var suffixRank[i]   will hold the rank (i.e. position) of the i-th suffix in SA.
+ * @var rankStart[r]    will hold the first position in SA where rank r appear.
+ * @var SA[i]           will hold the new suffix sorting of the current iteration.
+ *
+ * @param h             the current sort iteration of the suffix array.
+ */
+void computeSuffixRanks(int h) {
+    // Initialize a comparator on the first 2h letters
+    comparator comp(h);
+
+    // Compute the new rank of each suffix,
+    // and where each of these rank firstly appear in the suffix array
+    for (int i = 1; i < n; ++i) {
+        int& r = sortedRanks[i] = sortedRanks[i - 1];
+
+        // If the two suffixes differ in their first 2h letters
+        // then increment the rank and set its start position
+        if (comp(sortedSA[i - 1], sortedSA[i])) {
+            rankStart[++r] = i;
+        }
+    }
+
+    // Copy the sorted suffixes of the h-th iteration in the global suffix array,
+    // and re-map ranks into suffixRank array to be indexed by the suffix id
+    for (int i = 0; i < n; ++i) {
+        SA[i] = sortedSA[i];
+        suffixRank[SA[i]] = sortedRanks[i];
+    }
+}
 
 /**
  * Builds the suffix array of the given string in time complexity of
  * O(n.log(n)), where n is the length of the given string.
  *
  * Upon calling this function:
- * @var SA[i]       will hold the i-th smallest suffix of the string
- * @var bucket[i]   will hold the position of the i-th suffix in
- *                  suffix array (i.e. bucket[SA[i]] = i)
+ * @var SA[i]           will hold the i-th smallest suffix of the string.
+ * @var suffixRank[i]   will hold the position of the i-th suffix in
+ *                      suffix array (i.e. suffixRank[SA[i]] = i).
  *
- * @param str       the string needed to compute its suffix array.
+ * @param str           the string needed to compute its suffix array.
  */
 void buildSuffixArray(const string& str) {
-    // Number of suffixes
-    int n = str.size() + 1;
+    // Set the number of suffixes
+    n = str.size() + 1;
 
-    // Initially fill the suffixes and let their buckets be the first char in each suffix
-    for (int i = 0; i <= n; ++i) {
-        SA[i] = i;
-        bucket[i] = str[i];
+    // Initially fill the suffixes and let their ranks be the first char in each suffix
+    for (int i = 0; i < n; ++i) {
+        sortedSA[i] = i;
+        suffixRank[i] = str[i];
     }
 
-    // Sort suffixes on the first letter
-    sort(SA, SA + n, comparator(0));
+    // Sort suffixes on the first letter and initialize suffix ranks
+    sort(sortedSA, sortedSA + n, comparator(0));
+    computeSuffixRanks(0);
 
-    // Temporary arrays needed in the following calculations
-    vector<int> sortedSA(n, n - 1), sortedBucket(n), bucketStart(n);
-
-    // Keep sorting until the number of buckets become equals to the number of suffixes
-    for (int h = 1; sortedBucket[n - 1] != n - 1; h <<= 1) {
-        // Compute the new buckets of the previous sorting iteration
-        // During the h-th iteration, all the suffixes begin with the the same h letters
-        // will be in the same bucket (i.e. have the same bucket id)
-        comparator comp(h >> 1);
-        for (int i = 1; i < n; ++i) {
-            sortedBucket[i] = sortedBucket[i - 1] + comp(SA[i - 1], SA[i]);
-        }
-
-        // Compute where each bucket starts,
-        // and re-map buckets to be indexed by suffix id
-        for (int i = 1; i < n; ++i) {
-            bucket[SA[i]] = sortedBucket[i];
-
-            if (sortedBucket[i] > sortedBucket[i - 1]) {
-                bucketStart[sortedBucket[i]] = i;
-            }
-        }
-
+    // Keep sorting until the number of ranks become equals to the number of suffixes
+    for (int h = 1; sortedRanks[n - 1] != n - 1; h <<= 1) {
         // Sort the suffixes on their first 2h letters in a temporary array (i.e. sortedSA),
         // given they are sorted on the first h letters
         for (int i = 0; i < n; ++i) {
             int k = SA[i] - h;
 
             if (k >= 0) {
-                sortedSA[bucketStart[bucket[k]]++] = k;
+                sortedSA[rankStart[suffixRank[k]]++] = k;
             }
         }
 
-        // Copy the sorted suffix of the h-th iteration in the global suffix array
-        for (int i = 0; i < n; ++i) {
-            SA[i] = sortedSA[i];
-        }
+        // Compute intial suffix rank, rank start position,
+        // and copy the sorted suffix array into the global suffix array
+        computeSuffixRanks(h);
+    }
+}
+
+/**
+ * Computes the longest common prefix (LCP) of every two adjacent suffixes in the
+ * suffix array.
+ *
+ * Note that buildSuffixArray(str) must be called before calling this function.
+ *
+ * Upon calling this function:
+ * @var LCP[i]      will hold the length of the LCP between SA[i] and SA[i - 1].
+ *
+ * @param str       the string needed to compute its longest common prefix array.
+ */
+void buildLCP(const string& str) {
+    int cnt = 0;
+    for (int i = 0, k = 0; i < str.size(); ++i) {
+        int j = SA[suffixRank[i] - 1];
+        while (str[i + cnt] == str[j + cnt]) ++cnt;
+        LCP[suffixRank[i]] = cnt;
+        if (cnt > 0) --cnt;
     }
 }
 
@@ -109,9 +150,10 @@ int main() {
     cin >> s;
 
     buildSuffixArray(s);
+    buildLCP(s);
 
     for (int i = 0; i <= s.size(); ++i) {
-        cout << SA[i] << ' ' << s.substr(SA[i]) << endl;
+        cout << SA[i] << ' ' << s.substr(SA[i]) << ' ' << LCP[i] << endl;
     }
 
     return 0;
--- strings/suffix_array/suffix_array_slow.cpp
+++ strings/suffix_array/suffix_array_slow.cpp
@@ -17,15 +17,16 @@ using namespace std;
 // The maximum length of the string
 const int N = 10010;
 
-// SA:      the suffix array, holding all the suffixes in lexicographical order.
-// bucket:  array holding the bucket (i.e. the order) of the i-th suffix after sorting.
-int SA[N], bucket[N];
+// SA:          the suffix array, holding all the suffixes in lexicographical order.
+// suffixRank:  array holding the rank (i.e. the order) of the i-th suffix after sorting.
+// LCP:         array holding the length of the longest common prefix between SA[i] and SA[i - 1].
+int SA[N], suffixRank[N], LCP[N];
 
 /**
  * Comparator struct needed to sort the suffixes on the first 2h,
  * given they are already sorted on the first h letters.
  *
- * This struct uses bucket array in its comparisons.
+ * This struct uses suffixRank array in its comparisons.
  */
 struct comparator {
     int h;
@@ -33,9 +34,9 @@ struct comparator {
     comparator(int h) : h(h) {}
 
     bool operator()(int i, int j) const {
-        if (bucket[i] != bucket[j])
-            return bucket[i] < bucket[j];
-        return bucket[i + h] < bucket[j + h];
+        if (suffixRank[i] != suffixRank[j])
+            return suffixRank[i] < suffixRank[j];
+        return suffixRank[i + h] < suffixRank[j + h];
     }
 };
 
@@ -44,30 +45,30 @@ struct comparator {
  * O(n.log^2(n)), where n is the length of the given string.
  *
  * Upon calling this function:
- * @var SA[i]       will hold the i-th smallest suffix of the string
- * @var bucket[i]   will hold the position of the i-th suffix in
- *                  suffix array (i.e. bucket[SA[i]] = i)
+ * @var SA[i]           will hold the i-th smallest suffix of the string
+ * @var suffixRank[i]   will hold the position of the i-th suffix in
+ *                      suffix array (i.e. suffixRank[SA[i]] = i)
  *
- * @param str       the string needed to compute its suffix array.
+ * @param str           the string needed to compute its suffix array.
  */
 void buildSuffixArray(const string& str) {
     // Number of suffixes
     int n = str.size() + 1;
 
-    // Initially fill the suffixes and let their buckets be the first char in each suffix
+    // Initially fill the suffixes and let their ranks be the first char in each suffix
     for (int i = 0; i < n; ++i) {
         SA[i] = i;
-        bucket[i] = str[i];
+        suffixRank[i] = str[i];
     }
 
     // Sort suffixes on the first letter
     sort(SA, SA + n, comparator(0));
 
-    // Temporary bucket array needed in the following calculations
-    vector<int> sortedBucket(n + 1, 0);
+    // Temporary sorted ranks array needed in the following calculations
+    vector<int> sortedRanks(n, 0);
 
     // Keep sorting until the number of buckets become equals to the number of suffixes
-    for (int h = 1; sortedBucket[n - 1] != n - 1; h <<= 1) {
+    for (int h = 1; sortedRanks[n - 1] != n - 1; h <<= 1) {
         // Initialize a comparator on the first 2h letters
         comparator comp(h);
 
@@ -75,15 +76,36 @@ void buildSuffixArray(const string& str) {
         // given they are sorted in the first h letters
         sort(SA, SA + n, comp);
 
-        // Compute the new buckets of the suffixes after sorting on the first 2h letters.
+        // Compute the new ranks of the suffixes after sorting on the first 2h letters.
         for (int i = 1; i < n; ++i) {
-            sortedBucket[i] = sortedBucket[i - 1] + comp(SA[i - 1], SA[i]);
+            sortedRanks[i] = sortedRanks[i - 1] + comp(SA[i - 1], SA[i]);
         }
 
-        // Re-map buckets into bucket array to be indexed by the suffix id
+        // Re-map ranks into suffixRank array to be indexed by the suffix id
         for (int i = 0; i < n; ++i) {
-            bucket[SA[i]] = sortedBucket[i];
+            suffixRank[SA[i]] = sortedRanks[i];
         }
+    }
+}
+
+/**
+ * Computes the longest common prefix (LCP) of every two adjacent suffixes in the
+ * suffix array.
+ *
+ * Note that buildSuffixArray(str) must be called before calling this function.
+ *
+ * Upon calling this function:
+ * @var LCP[i]      will hold the length of the LCP between SA[i] and SA[i - 1].
+ *
+ * @param str       the string needed to compute its longest common prefix array.
+ */
+void buildLCP(const string& str) {
+    int cnt = 0;
+    for (int i = 0, k = 0; i < str.size(); ++i) {
+        int j = SA[suffixRank[i] - 1];
+        while (str[i + cnt] == str[j + cnt]) ++cnt;
+        LCP[suffixRank[i]] = cnt;
+        if (cnt > 0) --cnt;
     }
 }
 
@@ -93,9 +115,10 @@ int main() {
     cin >> s;
 
     buildSuffixArray(s);
+    buildLCP(s);
 
     for (int i = 0; i <= s.size(); ++i) {
-        cout << SA[i] << ' ' << s.substr(SA[i]) << endl;
+        cout << SA[i] << ' ' << s.substr(SA[i]) << ' ' << LCP[i] << endl;
     }
 
     return 0;
