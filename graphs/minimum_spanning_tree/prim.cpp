2cc4d24a412b3ecc58a2335873275f66a0805b32
Omar <omar_bazaraa@hotmail.com>
2019-03-27 15:48:54 +0200

    Fix Prim's algorithm for minimum spanning tree

--- graphs/minimum_spanning_tree/prim.cpp
+++ graphs/minimum_spanning_tree/prim.cpp
@@ -8,15 +8,15 @@ const int N = 100100;
 // Struct holds all needed information about the edges
 // with comparison operator defined for the priority queue sorting.
 struct edge {
-    int to, weight, weightSum;
+    int to, weight;
 
     edge() {}
-    edge(int t, int w, int s = 0) : to(t), weight(w), weightSum(s) {}
+    edge(int t, int w) : to(t), weight(w) {}
 
     // Note that we are actually implementing the greater than operator (not the less than operator)
     // as the priority queue is implemented as max heap.
     bool operator<(const edge& rhs) const {
-        return weightSum > rhs.weightSum;
+        return weight > rhs.weight;
     }
 };
 
@@ -26,27 +26,26 @@ vector<edge> edges[N];
 
 // Returns the total weight of the minimum spanning tree of the given weighted graph.
 // O(n.log(n))
-void primMST() {
+int primMST() {
     priority_queue<edge> q;
-    q.push(edge(src, 0));
+    q.push(edge(0, 0));
 
     int MST = 0;
 
     while (!q.empty()) {
         int u = q.top().to;
         int w = q.top().weight;
-        int s = q.top().weightSum;
         q.pop();
 
         if (vis[u]++) {
             continue;
         }
-
+        
         MST += w;
 
         for (edge& e : edges[u]) {
             if (!vis[e.to]) {
-                q.push(edge(e.to, e.weight, s + e.weight));
+                q.push(e);
             }
         }
     }
