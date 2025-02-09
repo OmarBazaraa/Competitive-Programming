02a5af8aef3629468d6f36cdec72b8cf2e267c40
Omar Bazaraa <ombazara@microsoft.com>
2025-02-09 07:22:45 +0200

    Refactor fenwick_tree class

--- src/data_structures/fenwick_tree/fenwick_tree.h
+++ src/data_structures/fenwick_tree/fenwick_tree.h
@@ -2,41 +2,38 @@
 
 /**
  * The `fenwick_tree` (a.k.a. Binary Indexed Tree) is a data structure that stores an array of values
- * and can efficiently update the values and compute prefix sums.
+ * and can efficiently update values and compute prefix sums.
  *
  * @note
  * The tree is is 1-indexed.
  *
  * @tparam T the type of the elements in the tree.
+ * @tparam N the maximum capacity of the tree.
  */
-template<class T>
+template<typename T, unsigned int N>
 class fenwick_tree {
-    int capacity;
-    std::vector<T> BIT;
+    int BIT[N + 1];
 
 public:
     /**
 	 * Constructs a new object of the `fenwick_tree` class with a fixed capacity.
      *
-     * Time complexity: `O(capacity)`.
-	 *
-	 * @param cap the capacity of the tree.
+     * Time complexity: `O(N)`.
 	 */
-    fenwick_tree(int cap) {
-        capacity = cap;
-        BIT.resize(capacity + 1, 0);
+    fenwick_tree() {
+        memset(BIT, 0, sizeof(BIT));
     }
 
     /**
      * Updates the value of a specified element in the tree by a given amount.
      *
-     * Time complexity: `O(log(capacity))`.
+     * Time complexity: `O(log(N))`.
      *
-     * @param idx the index of the element to update. Should be in the range of [1, capacity].
+     * @param idx the index of the element to update. Should be in the range of `[1, N]`.
      * @param val the amount to add to the specified element.
      */
     void update(int idx, T val) {
-        while (idx <= capacity) {
+        while (idx <= N) {
             BIT[idx] += val;
             idx += idx & -idx;
         }
@@ -45,10 +42,10 @@ public:
     /**
      * Computes the prefix sum of values in the tree up to a specified index.
      *
-     * Time complexity: `O(log(capacity))`.
+     * Time complexity: `O(log(N))`.
      *
      * @param idx the index of the last element in the prefix sum.
-     * @return the sum of values in the interval [1, idx].
+     * @return the sum of values in the interval `[1, idx]`.
      */
     T prefix_sum(int idx) const {
         T res = 0;
