cebedf20360ffcf6ea9865dcf09fd09da5d74b62
Omar Bazaraa <ombazara@microsoft.com>
2025-02-12 21:36:31 +0200

    Unify monotonic_queue tests using TYPED_TEST_SUITE

--- tests/data_structures/monotonic_queue_tests.cpp
+++ tests/data_structures/monotonic_queue_tests.cpp
@@ -1,40 +1,52 @@
 #include <gtest/gtest.h>
 
 #include "data_structures/monotonic_queue/monotonic_queue.h"
+#include "data_structures/monotonic_queue/monotonic_queue_using_stacks.h"
 
-TTEST(MonotonicQueueTest, InitialSetup) {
-    // Arrange.
-    monotonic_queue<int> mq;
+// A fixture class template for testing monotonic queues.
+template <typename T>
+class MonotonicQueue : public testing::Test {
+public:
+    T mq;
+};
 
-    //
-    // Assert
-    //
+// The list of types to test.
+using MyTypes = ::testing::Types<
+    monotonic_queue<int>,
+    monotonic_queue_using_stacks<int>
+>;
 
+// Define the test suite.
+TYPED_TEST_SUITE(MonotonicQueue, MyTypes);
+
+//
+// Test Cases
+//
+
+TYPED_TEST(MonotonicQueue, InitialSetup) {
     // Initially the queue should be empty.
-    EXPECT_TRUE(mq.empty());
-    EXPECT_EQ(mq.size(), 0);
+    EXPECT_TRUE(this->mq.empty());
+    EXPECT_EQ(this->mq.size(), 0);
 }
 
-TTEST(MonotonicQueueTest, PushSingleElement) {
+TYPED_TEST(MonotonicQueue, PushSingleElement) {
     // Arrange.
     int value = 5;
-    monotonic_queue<int> mq;
 
     // Act.
-    mq.push(value);
+    this->mq.push(value);
 
     // Assert.
-    EXPECT_FALSE(mq.empty());
-    EXPECT_EQ(mq.size(), 1);
-    EXPECT_EQ(mq.front(), value);
-    EXPECT_EQ(mq.max(), value);
-    EXPECT_EQ(mq.min(), value);
+    EXPECT_FALSE(this->mq.empty());
+    EXPECT_EQ(this->mq.size(), 1);
+    EXPECT_EQ(this->mq.front(), value);
+    EXPECT_EQ(this->mq.max(), value);
+    EXPECT_EQ(this->mq.min(), value);
 }
 
-TTEST(MonotonicQueueTest, PushMultipleElements) {
+TYPED_TEST(MonotonicQueue, PushMultipleElements) {
     // Arrange.
     std::vector<int> values = { 2, 5, 1, 3, 1, 6, 1, 7 };
-    monotonic_queue<int> mq;
 
     //
     // Act & Assert
@@ -45,43 +57,41 @@ TTEST(MonotonicQueueTest, PushMultipleElements) {
 
     for (int i = 0; i < values.size(); ++i) {
         // Push the value to the queue.
-        mq.push(values[i]);
+        this->mq.push(values[i]);
 
         // Update expected max & min.
         expected_max = std::max(expected_max, values[i]);
         expected_min = std::min(expected_min, values[i]);
 
         // Assert the size of the queue.
-        EXPECT_EQ(mq.size(), i + 1);
+        EXPECT_EQ(this->mq.size(), i + 1);
 
         // Assert the front element.
-        EXPECT_EQ(mq.front(), values[0]);
+        EXPECT_EQ(this->mq.front(), values[0]);
 
         // Assert the maximum & minimum values.
-        EXPECT_EQ(mq.max(), expected_max);
-        EXPECT_EQ(mq.min(), expected_min);
+        EXPECT_EQ(this->mq.max(), expected_max);
+        EXPECT_EQ(this->mq.min(), expected_min);
     }
 }
 
-TTEST(MonotonicQueueTest, PopSingleElement) {
+TYPED_TEST(MonotonicQueue, PopSingleElement) {
     // Arrange.
     int value = 5;
-    monotonic_queue<int> mq;
 
     // Act.
-    mq.push(value);
-    auto popped_value = mq.pop();
+    this->mq.push(value);
+    auto popped_value = this->mq.pop();
 
     // Assert.
     EXPECT_EQ(popped_value, value);
-    EXPECT_EQ(mq.size(), 0);
-    EXPECT_TRUE(mq.empty());
+    EXPECT_EQ(this->mq.size(), 0);
+    EXPECT_TRUE(this->mq.empty());
 }
 
-TTEST(MonotonicQueueTest, PopMultipleElements) {
+TYPED_TEST(MonotonicQueue, PopMultipleElements) {
     // Arrange.
     std::vector<int> values = { 2, 5, 1, 5, 1, 1, 1, 7 };
-    monotonic_queue<int> mq;
 
     //
     // Act & Assert
@@ -89,25 +99,24 @@ TTEST(MonotonicQueueTest, PopMultipleElements) {
 
     // Push the values to the queue.
     for (int i = 0; i < values.size(); ++i) {
-        mq.push(values[i]);
+        this->mq.push(values[i]);
     }
 
     // Pop the elements from the queue.
     for (int i = 0; i < values.size(); ++i) {
-        auto popped_value = mq.pop();
+        auto popped_value = this->mq.pop();
 
         EXPECT_EQ(popped_value, values[i]);
     }
 
     // Assert the queue is empty.
-    EXPECT_EQ(mq.size(), 0);
-    EXPECT_TRUE(mq.empty());
+    EXPECT_EQ(this->mq.size(), 0);
+    EXPECT_TRUE(this->mq.empty());
 }
 
-TTEST(MonotonicQueueTest, GetMaxAndMinValues) {
+TYPED_TEST(MonotonicQueue, GetMaxAndMinValues) {
     // Arrange.
     std::vector<int> values = { 2, 5, 1, 3, 6, 2, 1, 4, 1, 1 };
-    monotonic_queue<int> mq;
 
     //
     // Act & Assert
@@ -115,7 +124,7 @@ TTEST(MonotonicQueueTest, GetMaxAndMinValues) {
 
     // Push the values to the queue.
     for (int i = 0; i < values.size(); ++i) {
-        mq.push(values[i]);
+        this->mq.push(values[i]);
     }
 
     // Assert max and min values while popping from the queue.
@@ -123,17 +132,16 @@ TTEST(MonotonicQueueTest, GetMaxAndMinValues) {
         auto expected_max = *std::max_element(values.begin() + i, values.end());
         auto expected_min = *std::min_element(values.begin() + i, values.end());
 
-        EXPECT_EQ(mq.max(), expected_max);
-        EXPECT_EQ(mq.min(), expected_min);
+        EXPECT_EQ(this->mq.max(), expected_max);
+        EXPECT_EQ(this->mq.min(), expected_min);
 
-        mq.pop();
+        this->mq.pop();
     }
 }
 
-TTEST(MonotonicQueueTest, GetMaxAndMinValues_NonIncreasingElements) {
+TYPED_TEST(MonotonicQueue, GetMaxAndMinValues_NonIncreasingElements) {
     // Arrange.
     std::vector<int> values = { 6, 5, 5, 5, 5, 5, 3, 3, 3, 2, 1 };
-    monotonic_queue<int> mq;
 
     //
     // Act & Assert
@@ -141,7 +149,7 @@ TTEST(MonotonicQueueTest, GetMaxAndMinValues_NonIncreasingElements) {
 
     // Push the values to the queue.
     for (int i = 0; i < values.size(); ++i) {
-        mq.push(values[i]);
+        this->mq.push(values[i]);
     }
 
     // Assert max and min values while popping from the queue.
@@ -149,17 +157,16 @@ TTEST(MonotonicQueueTest, GetMaxAndMinValues_NonIncreasingElements) {
         auto expected_max = *std::max_element(values.begin() + i, values.end());
         auto expected_min = *std::min_element(values.begin() + i, values.end());
 
-        EXPECT_EQ(mq.max(), expected_max);
-        EXPECT_EQ(mq.min(), expected_min);
+        EXPECT_EQ(this->mq.max(), expected_max);
+        EXPECT_EQ(this->mq.min(), expected_min);
 
-        mq.pop();
+        this->mq.pop();
     }
 }
 
-TTEST(MonotonicQueueTest, GetMaxAndMinValues_NonDecreasingElements) {
+TYPED_TEST(MonotonicQueue, GetMaxAndMinValues_NonDecreasingElements) {
     // Arrange.
     std::vector<int> values = { 1, 1, 1, 2, 3, 3, 5, 5, 5, 5, 6, 7, 7, 9 };
-    monotonic_queue<int> mq;
 
     //
     // Act & Assert
@@ -167,7 +174,7 @@ TTEST(MonotonicQueueTest, GetMaxAndMinValues_NonDecreasingElements) {
 
     // Push the values to the queue.
     for (int i = 0; i < values.size(); ++i) {
-        mq.push(values[i]);
+        this->mq.push(values[i]);
     }
 
     // Assert max and min values while popping from the queue.
@@ -175,9 +182,9 @@ TTEST(MonotonicQueueTest, GetMaxAndMinValues_NonDecreasingElements) {
         auto expected_max = *std::max_element(values.begin() + i, values.end());
         auto expected_min = *std::min_element(values.begin() + i, values.end());
 
-        EXPECT_EQ(mq.max(), expected_max);
-        EXPECT_EQ(mq.min(), expected_min);
+        EXPECT_EQ(this->mq.max(), expected_max);
+        EXPECT_EQ(this->mq.min(), expected_min);
 
-        mq.pop();
+        this->mq.pop();
     }
 }
